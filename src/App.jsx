bb6786b60f8ce82564e50f65de60ca739b77bb56
andreyk1n <betas23wr@gmail.com>
2025-09-05 23:08:52 +0300

    update footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 // src/App.jsx
 import React from "react";
 import Header from "./template-parts/partials/Header";
+import Footer from "./template-parts/partials/Footer";
 
 function App() {
   return (
     <div>
       <Header />
+      <div style={{ width: "100%", height: "500px", background: 'gray', padding: '150px', }}>empty block</div>
+      <Footer />
     </div>
   );
 }
---
+++ src/template-parts/partials/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return ( 
+        <footer className="footer">
+            <div className="footer__container">
+                <div className="footer__info">footer</div>
+                <div className="footer__nav"></div>
+                <div className="footer__news"></div>
+            </div>
+        </footer>
+     );
+}
+ 
+export default Footer;
